a96ee5140731a20b369881121e3ad09cd822f14e
BRANDY <120415871+heejinki@users.noreply.github.com>
2024-03-02 18:39:37 +0900

    Design: Divider 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Divider from '@/components/UI/Divider/Divider';
+
 export default function Home() {
   return (
     <>
@@ -7,6 +9,7 @@ export default function Home() {
       <h4>타이포 그래피 테스트 h4</h4>
       <h5>타이포 그래피 테스트 h5</h5>
       <h6>타이포 그래피 테스트 h6</h6>
+      <Divider />
     </>
   );
 }
---
+++ src/components/UI/Divider/Divider.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Divider.module.scss';
+
+export default function Divider() {
+  return <hr className={styles.divider} />;
+}
