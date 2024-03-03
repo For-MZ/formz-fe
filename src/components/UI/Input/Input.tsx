401f8745c193f05843ec647fddc1933eb7b393b6
BRANDY <120415871+heejinki@users.noreply.github.com>
2024-03-03 22:52:28 +0900

    Design: Input test

---
+++ src/components/UI/Input/Input.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Input.module.scss';
+
+export default function Input() {
+  return (
+    <>
+      <input className={styles.input}></input>
+    </>
+  );
+}
