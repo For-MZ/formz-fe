e8e8f71340a91cb0cca04d940cd9eea0b6932de8
강동욱 <ehddnr8563@gmail.com>
2024-03-05 04:22:18 +0900

    Design: 헤더 UI 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -29,17 +29,19 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <nav className={styles.navigation}>
-        <Link href={'/'}>로고</Link>
         <ul className={styles.navMenu}>
+          <Link className={styles.navMenuLink} href={'/'}>
+            로고
+          </Link>
           {navMenu.map(({ href, name }) => (
             <li key={href}>
-              <Link href={href} className={`${pathName === href && styles.selected}`}>
+              <Link href={href} className={`${styles.navMenuLink} ${pathName === href && styles.selected}`}>
                 {name}
               </Link>
             </li>
           ))}
         </ul>
-        <Link href={'/login'} className={`${pathName === '/login' && styles.selected}`}>
+        <Link href={'/login'} className={`${styles.navMenuLink} ${pathName === '/login' && styles.selected}`}>
           로그인
         </Link>
       </nav>
