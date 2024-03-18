41b9400188622084716c1a5e9576876b19f28240
BRANDY <kimhijin1@naver.com>
2024-03-18 18:03:38 +0900

    Design: 마이페이지 CSS 수정

--- src/app/(mypage)/mypage/(bookmark)/bookmark/community/page.tsx
+++ src/app/(mypage)/mypage/(bookmark)/bookmark/community/page.tsx
@@ -1,13 +1,126 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import styles from './community.module.scss';
-import CommunityContent from '@/app/(mypage)/_components/CommunityCcontent';
+import PostList from '@/app/community/_components/FilterablePosts/PostList';
+import Pagination from '@/components/UI/Pagination';
+import { SimplePost } from '@/types/post';
 
 export default function community() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const posts: SimplePost[] = [
+    {
+      postId: '1',
+      title: '게시글 제목1',
+      author: '강동욱',
+      createdAt: '1시간전',
+      views: 1000,
+      likes: 5,
+      commentCount: 5,
+      category: '정책',
+    },
+    {
+      postId: '2',
+      title: '게시글 제목2',
+      author: '김희진',
+      createdAt: '2시간전',
+      views: 1000,
+      likes: 5,
+      commentCount: 5,
+      category: '공간',
+    },
+    {
+      postId: '3',
+      title: '게시글 제목3',
+      author: '강수정',
+      createdAt: '3시간전',
+      views: 1000,
+      likes: 5,
+      commentCount: 5,
+      category: '주택',
+    },
+    {
+      postId: '4',
+      title: '게시글 제목4',
+      author: '유성영',
+      createdAt: '4시간전',
+      views: 1000,
+      likes: 5,
+      commentCount: 5,
+      category: '취업',
+    },
+    {
+      postId: '5',
+      title: '게시글 제목5',
+      author: '박인서',
+      createdAt: '5시간전',
+      views: 1000,
+      likes: 5,
+      commentCount: 5,
+      category: '창업',
+    },
+    {
+      postId: '6',
+      title: '게시글 제목6',
+      author: '이규현',
+      createdAt: '6시간전',
+      views: 1000,
+      likes: 5,
+      commentCount: 5,
+      category: '자유',
+    },
+    {
+      postId: '7',
+      title: '게시글 제목7',
+      author: '이은지',
+      createdAt: '7시간전',
+      views: 1000,
+      likes: 5,
+      commentCount: 5,
+      category: '꿀팁',
+    },
+    {
+      postId: '8',
+      title: '게시글 제목8',
+      author: '강동욱',
+      createdAt: '1시간전',
+      views: 1000,
+      likes: 5,
+      commentCount: 5,
+      category: '정책',
+    },
+    {
+      postId: '9',
+      title: '게시글 제목9',
+      author: '강동욱',
+      createdAt: '1시간전',
+      views: 1000,
+      likes: 5,
+      commentCount: 5,
+      category: '정책',
+    },
+    {
+      postId: '10',
+      title: '게시글 제목10',
+      author: '강동욱',
+      createdAt: '1시간전',
+      views: 1000,
+      likes: 5,
+      commentCount: 5,
+      category: '정책',
+    },
+  ];
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className={styles.container}>
-      커뮤니티
       <div>
-        <CommunityContent />
+        <PostList posts={posts} />
+        <Pagination totalPages={10} currentPage={currentPage} onPageChange={handlePageChange} />
       </div>
     </div>
   );
