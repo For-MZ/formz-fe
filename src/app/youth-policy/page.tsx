0b2b52ce9c4b3d901f08bafb89406f7496239949
강수정 <kngsujng@naver.com>
2024-03-18 22:16:11 +0900

    Design: FilterablePolicies 공통 컴포넌트 마크업

--- src/app/youth-policy/page.tsx
+++ src/app/youth-policy/page.tsx
@@ -1,9 +1,12 @@
+import styles from './page.module.scss';
 import Filter from './_components/Filter';
+import FilterablePolicies from './_components/FilterablePolicies';
 
 export default function PolicyPage() {
   return (
-    <>
+    <section className={styles.pageWrapper}>
       <Filter />
-    </>
+      <FilterablePolicies />
+    </section>
   );
 }
