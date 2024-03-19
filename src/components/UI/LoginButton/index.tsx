660d652a2dbb7cae969dfbf79916e1b4a2299de3
강수정 <kngsujng@naver.com>
2024-03-19 22:44:47 +0900

    Fix: LoginButton svg파일 안 보이는 문제 해결

--- src/components/UI/LoginButton/index.tsx
+++ src/components/UI/LoginButton/index.tsx
@@ -1,30 +1,29 @@
 'use client';
 
 import styles from './LoginButton.module.scss';
-import Image, { StaticImageData } from 'next/image';
-import kakaoIcon from '/public/icons/kakaotalk.svg';
-import googleIcon from '/public/icons/google.svg';
+import KakaoIcon from '/public/icons/kakaotalk.svg';
+import GoogleIcon from '/public/icons/google.svg';
 
 type ButtonType = 'default' | 'kakaoTalk' | 'google';
 type ButtonInfo = {
-  icon: string | StaticImageData;
+  Icon: undefined | React.ComponentType<React.SVGProps<SVGSVGElement>>;
   text: string;
 };
 const buttonInfo: Record<ButtonType, ButtonInfo> = {
-  default: { icon: '', text: '로그인' },
-  kakaoTalk: { icon: kakaoIcon, text: '카카오로 로그인' },
-  google: { icon: googleIcon, text: '구글로 로그인' },
+  default: { Icon: undefined, text: '로그인' },
+  kakaoTalk: { Icon: KakaoIcon, text: '카카오로 로그인' },
+  google: { Icon: GoogleIcon, text: '구글로 로그인' },
 };
 
 type Props = { type: ButtonType };
 
 export default function LoginButton({ type }: Props) {
   const handleLogin = () => {};
-  const { icon, text } = buttonInfo[type];
+  const { Icon, text } = buttonInfo[type];
 
   return (
     <button className={`${styles.loginBtn} ${styles[type]}`} onClick={handleLogin}>
-      {icon && <Image src={icon} alt={`${text} 아이콘`} className={`${styles.icon} ${styles[type]}`} />}
+      {Icon && <Icon className={`${styles.icon} ${styles[type]}`} />}
       <span className={`${styles.text} ${styles[type]}`}>{text}</span>
     </button>
   );
