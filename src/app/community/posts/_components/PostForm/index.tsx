d9a340e61cf73f61256037df35bfd2ec97e0a0da
강동욱 <ehddnr8563@gmail.com>
2024-03-20 18:51:34 +0900

    Fix: toast-editor 호환 이슈로 인한 react-quill 텍스트 에디터로 변경

--- src/app/community/posts/_components/PostForm/index.tsx
+++ src/app/community/posts/_components/PostForm/index.tsx
@@ -3,19 +3,21 @@
 import styles from './PostForm.module.scss';
 import DropDown from '@/components/UI/DropDown';
 import TextField from '@/components/UI/TextField';
-import useInput from '@/hooks/useInput';
 import { useEffect, useState } from 'react';
 import Button from '@/components/UI/Button';
 import dynamic from 'next/dynamic';
-const TextEditor = dynamic(() => import('./Editor'), { ssr: false }); // 에디터 ssr 지원안해서 다이나믹 임포트
+const TextEditor = dynamic(() => import('./TextEditor'), { ssr: false }); // 에디터 ssr 지원안해서 다이나믹 임포트
 
 const CATEGORY_OPTIONS = ['정책', '공간', '주택', '취업', '창업', '자유', '꿀팁'];
 
 export default function PostForm() {
-  const [title, handleChangeTitle] = useInput('');
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState<string | null>(null);
+  const [categoryError, setCategoryError] = useState('');
+  const [title, setTitle] = useState('');
+  const [titleError, setTitleError] = useState('');
   const [content, setContent] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [contentError, setContentError] = useState('');
+
   console.log(category);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -28,53 +30,67 @@ export default function PostForm() {
     // await axios.post('/community/posts', { selectedCategory, title });
   };
 
-  const handleSelectCategory = (option: string) => {
-    setCategory(option);
+  const handleSelectCategory = (selectedCategory: string) => {
+    setCategory(selectedCategory);
   };
 
   useEffect(() => {
-    handleValidation();
+    if (!category) {
+      setCategoryError('카테고리를 선택해야 합니다.');
+    } else {
+      setCategoryError('');
+    }
   }, [category]);
 
-  const handleValidation = () => {
-    if (category.length === 0) {
-      setErrorMessage('카테고리를 선택해주세요.');
-    } else {
-      setErrorMessage('');
+  const handleChangeTitle = (titleValue: string) => {
+    setTitle(titleValue);
+
+    if (title.length > 0 && title.length < 61) {
+      setTitleError('');
     }
   };
 
-  const handleChangeContent = (data: string) => {
-    setContent(data);
+  const handleChangeContent = (editorHtmlValue: string) => {
+    setContent(editorHtmlValue);
 
-    if (content.length < 10 && content.length > 1000) {
-      setErrorMessage('게시글 본문은 10~1000자 사이의 내용을 입력해주세요.');
+    if (content.length > 9 && content.length < 1001) {
+      setContentError('본문은 10자 이상 1000자 이하여야 합니다.');
     }
   };
 
   return (
     <form className={styles.postForm} onSubmit={handleSubmit}>
       <div className={styles.category}>
-        <label>카테고리</label>
+        <label htmlFor="category">카테고리</label>
         <DropDown
+          id="category"
           placeholder="카테고리를 선택해주세요."
           options={CATEGORY_OPTIONS}
-          selectedOption={category}
-          onSelect={handleSelectCategory}
+          onSelectProp={handleSelectCategory}
+          hasError={!!categoryError}
         />
-        {errorMessage && <p>{errorMessage}</p>}
+        {categoryError && <p className={`${!!categoryError && 'errorMessage'}`}>{categoryError}</p>}
       </div>
       <div className={styles.title}>
+        <label htmlFor="title">제목</label>
         <TextField
-          labelText="제목"
+          id="title"
           value={title}
-          onChange={handleChangeTitle}
+          onChangeProp={handleChangeTitle}
           placeholder="1~60자 사이의 제목을 입력해주세요."
+          hasError={!!titleError}
+          helpMessage={titleError}
+          onFocus={() => {
+            if (!(title.length > 0 && title.length < 61)) {
+              setTitleError('제목은 1자 이상 60자 이하여야 합니다.');
+            }
+          }}
         />
       </div>
       <div className={styles.content}>
-        <label>본문</label>
-        <TextEditor initialValue={content} onChange={handleChangeContent} />
+        <label htmlFor="content">본문</label>
+        <TextEditor id="content" value={content} onChange={handleChangeContent} />
+        {contentError && <p>{contentError}</p>}
       </div>
       <div className={styles.buttons}>
         <Button type="outline" text="취소" disabled={false} onClick={() => {}} />
