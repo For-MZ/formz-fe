36776a048ceda840a372a3eab6613ba61484e87b
강동욱 <ehddnr8563@gmail.com>
2024-03-21 01:03:47 +0900

    Feat: Button 컴포넌트 외부 컴포넌트에서 className prop으로 확장 가능하게 구현

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -11,26 +11,19 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   LeftIcon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   RightIcon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   text?: string;
+  className?: string;
 }
 
-export default function Button({
-  width = '125px',
-  height,
-  fontSize,
-  design,
-  LeftIcon,
-  RightIcon,
-  text,
-  ...props
-}: Props) {
+export default function Button({ design, LeftIcon, RightIcon, text, className, ...props }: Props) {
   return (
+    // ! 인라인 스타일 삭제, 외부 Button 컴포넌트 사용하는 곳에서 className 지정해서 사용! (Alert, Confirm 컴포넌트 참고)
     <button
-      style={{ minWidth: width, height, fontSize }}
-      className={`${styles.button} ${design === 'filled' ? styles.filled : design === 'outline' ? styles.outline : styles.transparent}`}
+      className={`${styles.button} ${className} ${design === 'filled' ? styles.filled : design === 'outline' ? styles.outline : styles.transparent}`}
       {...props}
     >
       {LeftIcon && <LeftIcon className={styles.icon} />}
-      <span className={styles.text}>{text}</span>
+      {/* ! 외부 컴포넌트에서 텍스트 사이즈 지정하기 위해 span 태그 없앴습니다. */}
+      {text}
       {RightIcon && <RightIcon className={styles.icon} />}
     </button>
   );
