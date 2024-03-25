2e48cce35e3a2ac3949e36ecdc4fdf1326640c6a
강동욱 <ehddnr8563@gmail.com>
2024-03-25 15:42:26 +0900

    Refactor: DropDown 수정

--- src/components/UI/DropDown/index.tsx
+++ src/components/UI/DropDown/index.tsx
@@ -1,39 +1,80 @@
 'use client';
 
-import { ButtonHTMLAttributes, useState } from 'react';
+import { ButtonHTMLAttributes, useEffect, useRef, useState } from 'react';
 import styles from './DropDown.module.scss';
 import UpIcon from '/public/icons/chevron-up.svg';
 import DownIcon from '/public/icons/chevron-down.svg';
 
 type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  id?: string;
   options: string[];
+  selectedValue?: string | null;
   onSelectProp: (selectedOption: string) => void;
   placeholder?: string;
   hasError?: boolean;
+  className?: string;
 };
 
-export default function DropDown({ options, onSelectProp, placeholder, hasError, ...buttonProps }: Props) {
-  const [isOpenOptionList, setIsOpenOptionList] = useState(false);
-  const [selectedOption, setSelectedOption] = useState<string | null>(placeholder ? null : options[0]);
+/**
+ *
+ * @param id button태그 id props
+ * @param className container className props
+ * @returns
+ */
+export default function DropDown({
+  options,
+  placeholder,
+  selectedValue,
+  onSelectProp,
+  hasError,
+  className,
+  ...buttonProps
+}: Props) {
+  const [selectedOption, setSelectedOption] = useState<string | null>(
+    selectedValue || (placeholder ? null : options[0]),
+  );
+  const [isOpenOptionList, setIsOpenOptionList] = useState<boolean>(false);
+  const optionListRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      // 드롭다운 메뉴 컨테이너 요소
+      const optionListElement = optionListRef.current as HTMLElement;
+
+      // 클릭된 요소가 드롭다운 메뉴 컨테이너 내부에 있는지 확인
+      if (!optionListElement.contains(event.target as Element)) {
+        // 외부 영역 클릭 시 메뉴 닫기
+        setIsOpenOptionList(false);
+      }
+    };
+
+    // 메뉴 열려 있을 때만 리스너 추가
+    if (isOpenOptionList) {
+      document.addEventListener('click', handleClickOutside);
+    }
+
+    // 컴포넌트 언마운트 시 리스너 제거
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, [isOpenOptionList]);
 
   const handleToggleDropDown = () => {
     setIsOpenOptionList((prev) => !prev);
   };
 
-  const handleClickOption = (selectedOption: string) => {
-    setSelectedOption(selectedOption);
-    onSelectProp?.(selectedOption);
+  const handleClickOption = (option: string) => {
+    setSelectedOption(option);
+    onSelectProp(option);
     setIsOpenOptionList(false);
   };
 
   return (
-    <div className={styles.selectContainer}>
+    <div className={`${styles.container} ${className}`}>
       {/* 드롭다운 토글링 버튼 */}
       <button
-        {...buttonProps}
         type="button"
         className={`${styles.toggle} ${hasError && styles.error}`}
         onClick={handleToggleDropDown}
+        {...buttonProps}
       >
         {selectedOption || <span className={styles.placeholder}>{placeholder}</span>}
         <div className={styles.icons}>
@@ -44,7 +85,7 @@ export default function DropDown({ options, onSelectProp, placeholder, hasError,
 
       {/* 드롭다운 토글링 버튼 클릭 시 보이는 옵션 리스트 */}
       {isOpenOptionList && (
-        <ul className={styles.optionList}>
+        <menu className={styles.optionList} ref={optionListRef}>
           {options.map((option) => (
             <li
               className={`${styles.optionItem} ${selectedOption === option && styles.selected}`}
@@ -54,7 +95,7 @@ export default function DropDown({ options, onSelectProp, placeholder, hasError,
               {option}
             </li>
           ))}
-        </ul>
+        </menu>
       )}
     </div>
   );
