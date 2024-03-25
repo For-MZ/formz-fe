a73420f84c08404edf014f9a03d941db9f377916
강수정 <kngsujng@naver.com>
2024-03-25 15:11:25 +0900

    Feat: 지역(시/도)에 따른 지역(시/군/구) 드롭다운 연동 및 기타 상태값 설정

---
+++ src/app/youth-place/_components/DistrictDropDowns/index.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import DropDown from '@/components/UI/DropDown';
+import { handleSubDistrictsChange } from '@/utils/subDistrict';
+
+type Props = {
+  district: string;
+  setDistrict: (selected: string) => void;
+  subDistrict: string;
+  setSubDistrict: (selected: string) => void;
+};
+
+const DISTRICT = [
+  '서울',
+  '부산',
+  '대구',
+  '인천',
+  '광주',
+  '대전',
+  '울산',
+  '경기',
+  '강원',
+  '충북',
+  '충남',
+  '전북',
+  '전남',
+  '경북',
+  '경남',
+  '제주',
+  '세종',
+];
+export default function DistrictDropDowns({ district, setDistrict, subDistrict, setSubDistrict }: Props) {
+  const SUBDISTRICT = handleSubDistrictsChange(district);
+  const [key, setKey] = useState(0);
+  useEffect(() => {
+    setKey((prevKey) => prevKey + 1);
+    setSubDistrict('');
+  }, [district]);
+  console.log(district, subDistrict);
+  return (
+    <>
+      <DropDown
+        options={DISTRICT}
+        onSelectProp={(selected: string) => setDistrict(selected)}
+        placeholder="지역(시/도)"
+      />
+      <DropDown
+        key={key}
+        options={SUBDISTRICT}
+        onSelectProp={(selected: string) => setSubDistrict(selected)}
+        placeholder="지역(시/군/구)"
+      />
+    </>
+  );
+}
