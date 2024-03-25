a80b944b5cbac7ab1fa6de665b851290d1f5447e
강동욱 <ehddnr8563@gmail.com>
2024-03-25 15:58:55 +0900

    Feat: PostForm 유효성 검사 로직 추가

--- src/app/community/posts/_components/PostForm/index.tsx
+++ src/app/community/posts/_components/PostForm/index.tsx
@@ -1,24 +1,30 @@
 'use client';
 
 import styles from './PostForm.module.scss';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import DropDown from '@/components/UI/DropDown';
 import TextField from '@/components/UI/TextField';
-import { useEffect, useState } from 'react';
 import Button from '@/components/UI/Button';
+import WriteIcon from '/public/icons/edit-2.svg';
 import dynamic from 'next/dynamic';
-const TextEditor = dynamic(() => import('./TextEditor'), { ssr: false }); // 에디터 ssr 지원안해서 다이나믹 임포트
+// react-quill ssr 지원안해서 다이나믹 임포트
+const TextEditor = dynamic(() => import('./TextEditor'), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+});
 
 const CATEGORY_OPTIONS = ['정책', '공간', '주택', '취업', '창업', '자유', '꿀팁'];
 
 export default function PostForm() {
-  const [category, setCategory] = useState<string | null>(null);
-  const [categoryError, setCategoryError] = useState('');
-  const [title, setTitle] = useState('');
-  const [titleError, setTitleError] = useState('');
-  const [content, setContent] = useState('');
-  const [contentError, setContentError] = useState('');
+  const navigate = useRouter();
 
-  console.log(category);
+  const [category, setCategory] = useState<string | null>(null);
+  const [categoryError, setCategoryError] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [titleError, setTitleError] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+  const [contentError, setContentError] = useState<string>('');
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -27,74 +33,105 @@ export default function PostForm() {
     // TODO 유효성 검사
 
     // TODO 게시글 생성 요청
+    console.log({ category, title, content });
+
     // await axios.post('/community/posts', { selectedCategory, title });
   };
 
   const handleSelectCategory = (selectedCategory: string) => {
     setCategory(selectedCategory);
-  };
 
-  useEffect(() => {
-    if (!category) {
-      setCategoryError('카테고리를 선택해야 합니다.');
-    } else {
+    if (selectedCategory) {
       setCategoryError('');
     }
-  }, [category]);
+  };
 
-  const handleChangeTitle = (titleValue: string) => {
-    setTitle(titleValue);
+  const handleChangeTitle = (value: string) => {
+    setTitle(value);
 
-    if (title.length > 0 && title.length < 61) {
+    if (value.length > 0 && value.length < 61) {
       setTitleError('');
+    } else {
+      setTitleError('제목은 1자 이상 60자 이하여야 합니다.');
     }
   };
 
-  const handleChangeContent = (editorHtmlValue: string) => {
-    setContent(editorHtmlValue);
+  const handleChangeContent = (htmlValue: string, contentValue: string) => {
+    setContent(htmlValue);
 
-    if (content.length > 9 && content.length < 1001) {
+    const contentValueLength = contentValue.length - 1;
+
+    if (contentValueLength > 9 && contentValueLength < 1001) {
+      setContentError('');
+    } else {
       setContentError('본문은 10자 이상 1000자 이하여야 합니다.');
     }
   };
 
   return (
     <form className={styles.postForm} onSubmit={handleSubmit}>
-      <div className={styles.category}>
-        <label htmlFor="category">카테고리</label>
+      <div className={styles.categoryBox}>
+        <label htmlFor="category" className={styles.categoryLabel}>
+          카테고리
+        </label>
         <DropDown
           id="category"
+          className={styles.categoryDropDown}
           placeholder="카테고리를 선택해주세요."
           options={CATEGORY_OPTIONS}
+          selectedValue={category}
           onSelectProp={handleSelectCategory}
           hasError={!!categoryError}
+          onBlur={() => {
+            if (!category) {
+              setCategoryError('카테고리를 선택해야 합니다.');
+            }
+          }}
         />
-        {categoryError && <p className={`${!!categoryError && 'errorMessage'}`}>{categoryError}</p>}
+        {categoryError && <p className={`${!!categoryError && styles.errorMessage}`}>{categoryError}</p>}
       </div>
-      <div className={styles.title}>
-        <label htmlFor="title">제목</label>
+      <div className={styles.titleBox}>
         <TextField
+          labelText="제목"
+          className={styles.titleInput}
+          labelClassName={styles.titleLabel}
           id="title"
-          value={title}
+          valueProp={title}
           onChangeProp={handleChangeTitle}
           placeholder="1~60자 사이의 제목을 입력해주세요."
           hasError={!!titleError}
           helpMessage={titleError}
-          onFocus={() => {
+          onBlur={() => {
             if (!(title.length > 0 && title.length < 61)) {
               setTitleError('제목은 1자 이상 60자 이하여야 합니다.');
             }
           }}
         />
       </div>
-      <div className={styles.content}>
-        <label htmlFor="content">본문</label>
-        <TextEditor id="content" value={content} onChange={handleChangeContent} />
-        {contentError && <p>{contentError}</p>}
+      <div className={styles.contentBox}>
+        <label htmlFor="content" className={styles.contentLabel}>
+          본문
+        </label>
+        <TextEditor
+          value={content}
+          onChangeProp={handleChangeContent}
+          placeholder="본문을 입력해주세요."
+          hasError={!!contentError}
+          onBlurProp={(textValue: string) => {
+            const textValueLength = textValue.length - 1;
+
+            if (textValueLength > 9 && textValueLength < 1001) {
+              setContentError('');
+            } else {
+              setContentError('본문은 10자 이상 1000자 이하여야 합니다.');
+            }
+          }}
+        />
+        {contentError && <p className={`${contentError && styles.errorMessage}`}>{contentError}</p>}
       </div>
-      <div className={styles.buttons}>
-        <Button type="outline" text="취소" disabled={false} onClick={() => {}} />
-        <Button type="filled" text="게시글 작성" disabled={false} onClick={() => {}} />
+      <div className={styles.buttonBox}>
+        <Button type="button" design="outline" text="취소" onClick={() => navigate.back()} />
+        <Button type="submit" design="filled" text="게시글 작성" LeftIcon={WriteIcon} />
       </div>
     </form>
   );
