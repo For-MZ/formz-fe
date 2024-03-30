40a740cf18926ed3867f639f171b1df2e34abb7a
BRANDY <kimhijin1@naver.com>
2024-03-30 16:41:13 +0900

    Feat: 회원가입 유효성 검사 제작완료 및 이미지 업로드 제작중

--- src/app/(hasNotMap)/signup/page.tsx
+++ src/app/(hasNotMap)/signup/page.tsx
@@ -25,6 +25,7 @@ type FormState = {
   verificationCode: string;
   verificationError: string;
   showPassword: boolean;
+  profileImage: File | string;
 };
 
 type ApiResponse = {
@@ -39,6 +40,8 @@ type SignupData = {
 };
 
 export default function Signup() {
+  const defaultProfileImage = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
+
   const initialFormState: FormState = {
     email: '',
     emailError: '',
@@ -55,15 +58,14 @@ export default function Signup() {
     verificationCode: '',
     verificationError: '',
     showPassword: false,
+    profileImage: defaultProfileImage, // 초기값: null
   };
 
   const [formState, setFormState] = useState<FormState>(initialFormState);
-
   const emailRegEx = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
   const nicknameRegEx = /^[a-zA-Z0-9가-힣]{2,10}$/;
   const passwordRegEx = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-  // 이미지를 클릭하는 함수 생성 및 비밀번호 보기 여부 토글 설정
-  // 이미지를 클릭하는 함수 생성 및 비밀번호 보기 여부 토글 설정
+
   const toggleShowPassword = () => {
     setFormState((prevState) => ({
       ...prevState,
@@ -82,11 +84,23 @@ export default function Signup() {
   };
 
   const handleMailAuthClick = async () => {
-    setFormState((prevState) => ({
-      ...prevState,
-      showEmailInput: true,
-    }));
-    requestVerificationCode(formState.email); // 인증 번호를 요청
+    if (emailRegEx.test(formState.email)) {
+      setFormState((prevState) => ({
+        ...prevState,
+        showEmailInput: true,
+      }));
+      requestVerificationCode(formState.email); // 인증 번호를 요청
+    } else if (formState.email.length < 1) {
+      setFormState((prevState) => ({
+        ...prevState,
+        emailError: '이메일을 입력해주세요.',
+      }));
+    } else if (!emailRegEx.test(formState.email)) {
+      setFormState((prevState) => ({
+        ...prevState,
+        emailError: '올바른 이메일 형식으로 입력해주세요.',
+      }));
+    }
   };
 
   const handleVerifyClick = async () => {
@@ -95,6 +109,11 @@ export default function Signup() {
       // 사용자가 입력한 인증번호와 백엔드에서 받은 인증번호를 비교
       if (formState.verificationCode === receivedVerificationCode) {
         console.log('Verification successful');
+        setFormState((prevState) => ({
+          ...prevState,
+          showEmailInput: false,
+          emailVerified: true,
+        }));
         // 인증 성공 처리
       } else {
         console.error('Verification failed');
@@ -112,16 +131,55 @@ export default function Signup() {
     }
   };
 
-  const handleSubmit = async (): Promise<ApiResponse> => {
-    console.log('Email:', formState.email);
-    console.log('Nickname:', formState.nickname);
-    console.log('Password:', formState.password);
-    console.log('ConfirmPassword:', formState.confirmPassword);
+  const handleSubmit = async (): Promise<ApiResponse | undefined> => {
+    // 필수 입력 항목 체크
+    if (!formState.email || !formState.nickname || !formState.password || !formState.confirmPassword) {
+      setFormState((prevState) => ({
+        ...prevState,
+        emailError: !formState.email ? '이메일을 입력해주세요.' : '',
+        nicknameError: !formState.nickname ? '닉네임을 입력해주세요.' : '',
+        passwordError: !formState.password ? '비밀번호를 입력해주세요.' : '',
+        confirmPasswordError: !formState.confirmPassword ? '비밀번호를 입력해주세요.' : '',
+      }));
+      return;
+    }
+
+    // 유효성 검사
+    const errors: Partial<FormState> = {};
+    if (!emailRegEx.test(formState.email)) errors.emailError = '올바른 이메일 형식으로 입력해주세요.';
+    if (!formState.emailVerified) errors.emailError = '메일 인증을 받아주세요.';
+    if (!nicknameRegEx.test(formState.nickname)) errors.nicknameError = '올바른 닉네임 형식으로 입력해주세요.';
+    if (!passwordRegEx.test(formState.password))
+      errors.passwordError = '영문 대소문자, 숫자, 특수 문자 포함 8자 이상 입력해주세요.';
+    if (formState.password !== formState.confirmPassword) errors.confirmPasswordError = '비밀번호가 일치하지 않습니다.';
+
+    if (Object.keys(errors).length > 0) {
+      setFormState((prevState) => ({ ...prevState, ...errors }));
+      return;
+    }
+    // FormData 객체 생성
+    const formData = new FormData();
+    formData.append('email', formState.email);
+    formData.append('nickname', formState.nickname);
+    formData.append('password', formState.password);
+
+    // 프로필 이미지가 있는 경우에만 FormData에 추가
+    if (formState.profileImage) {
+      formData.append('profileImage', formState.profileImage);
+    } else {
+      // 파일을 업로드하지 않은 경우 기본 프로필 이미지를 추가
+      formData.append('profileImage', defaultProfileImage);
+    }
 
     try {
-      const response: AxiosResponse<ApiResponse> = await axios.post<SignupData, AxiosResponse<ApiResponse>>(
-        '/api/sign-up', // 회원가입을 처리하는 API 엔드포인트의 경로를 지정해주세요
-        { email: formState.email, nickname: formState.nickname, password: formState.password }, // 회원가입 요청에 필요한 데이터
+      const response: AxiosResponse<ApiResponse> = await axios.post<FormData, AxiosResponse<ApiResponse>>(
+        '/api/sign-up', // 회원가입을 처리하는 API 엔드포인트의 경로
+        formData, // FormData 객체 전달
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data', // 파일 업로드 시 필수
+          },
+        },
       );
       return response.data; // 서버 응답을 반환합니다.
     } catch (error) {
@@ -139,19 +197,38 @@ export default function Signup() {
         setFormState((prevState) => ({
           ...prevState,
           nicknameAvailable: true,
+          nicknameError: '닉네임 사용 가능',
         }));
-        console.log('닉네임 사용 가능');
       } else {
         // 닉네임이 이미 사용 중인 경우
         setFormState((prevState) => ({
           ...prevState,
+          nicknameError: '닉네임이 이미 사용 중입니다.',
           nicknameAvailable: false,
         }));
-        console.log('닉네임이 이미 사용 중입니다.');
       }
     } catch (error) {
       // 오류 처리
-      console.error('닉네임 중복 확인 중 오류 발생:', error);
+      setFormState((prevState) => ({
+        ...prevState,
+        nicknameError: '닉네임 중복 확인 중 오류 발생',
+        nicknameAvailable: false,
+      }));
+    }
+  };
+
+  const handleProfileImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]; // 선택된 파일 가져오기
+    if (file) {
+      setFormState((prevState) => ({
+        ...prevState,
+        profileImage: file, // 상태 업데이트
+      }));
+    } else {
+      setFormState((prevState) => ({
+        ...prevState,
+        profileImage: defaultProfileImage, // 파일이 없는 경우 defaultProfileImage로 설정
+      }));
     }
   };
 
@@ -172,7 +249,7 @@ export default function Signup() {
             placeholder="ForMZ@example.com"
             onBlur={() => {
               if (formState.email.length < 1) {
-                setFormState((prevState) => ({ ...prevState, emailError: '이메일을 입력하세요.' }));
+                setFormState((prevState) => ({ ...prevState, emailError: '이메일을 입력해주세요.' }));
               } else if (formState.email.length > 0 && emailRegEx.test(formState.email)) {
                 setFormState((prevState) => ({ ...prevState, emailError: '' }));
               } else if (!emailRegEx.test(formState.email)) {
@@ -183,13 +260,18 @@ export default function Signup() {
         </div>
 
         <div>
-          <Button
-            className={styles.button}
-            design="outline"
-            onClick={handleMailAuthClick}
-            text="메일 인증"
-            disabled={false}
-          />
+          {!formState.emailVerified && (
+            <Button
+              className={styles.button}
+              design="outline"
+              onClick={handleMailAuthClick}
+              text="메일 인증"
+              disabled={false}
+            />
+          )}
+          {formState.emailVerified && (
+            <Button className={styles.button} design="outline" text="인증 완료" disabled={true} />
+          )}
         </div>
       </div>
 
@@ -199,8 +281,17 @@ export default function Signup() {
             <TextField
               className={styles.input}
               value={formState.verificationCode}
+              hasError={!!formState.verificationError}
+              helpMessage={formState.verificationError}
+              onBlur={() => {
+                if (formState.verificationCode.length < 1) {
+                  setFormState((prevState) => ({ ...prevState, verificationError: '인증번호를 입력해주세요.' }));
+                } else if (formState.verificationCode.length > 0) {
+                  setFormState((prevState) => ({ ...prevState, verificationError: '' }));
+                }
+              }}
               placeholder="인증 번호를 입력해주세요."
-              onChange={(e) => setFormState((prevState) => ({ ...prevState, verificationCode: e.target.value }))}
+              onChangeProp={(value) => setFormState((prevState) => ({ ...prevState, verificationCode: value }))}
             />
           </div>
 
@@ -213,9 +304,6 @@ export default function Signup() {
               onClick={handleVerifyClick}
             />
           </div>
-          {formState.verificationError && (
-            <div style={{ color: 'red', marginTop: '8px' }}>{formState.verificationError}</div>
-          )}
         </div>
       )}
       <div className={styles.name}>
@@ -232,7 +320,7 @@ export default function Signup() {
               maxLength={10}
               onBlur={() => {
                 if (formState.nickname.length < 1) {
-                  setFormState((prevState) => ({ ...prevState, nicknameError: '닉네임을 입력하세요.' }));
+                  setFormState((prevState) => ({ ...prevState, nicknameError: '닉네임을 입력해주세요.' }));
                 } else if (formState.nickname.length > 0 && nicknameRegEx.test(formState.nickname)) {
                   setFormState((prevState) => ({ ...prevState, nicknameError: '' }));
                 } else if (!nicknameRegEx.test(formState.nickname)) {
@@ -270,7 +358,7 @@ export default function Signup() {
           helpMessage={formState.passwordError}
           onBlur={() => {
             if (formState.password.length < 1) {
-              setFormState((prevState) => ({ ...prevState, passwordError: '비밀번호를 입력하세요.' }));
+              setFormState((prevState) => ({ ...prevState, passwordError: '비밀번호를 입력해주세요.' }));
             } else if (formState.password.length > 0 && passwordRegEx.test(formState.password)) {
               setFormState((prevState) => ({ ...prevState, passwordError: '' }));
             } else if (!passwordRegEx.test(formState.password)) {
@@ -293,22 +381,42 @@ export default function Signup() {
           hasError={!!formState.confirmPasswordError}
           helpMessage={formState.confirmPasswordError}
           onBlur={() => {
-            if (formState.password === formState.confirmPassword) {
-              setFormState((prevState) => ({ ...prevState, confirmPasswordError: '' }));
+            if (formState.confirmPassword.length < 1) {
+              setFormState((prevState) => ({ ...prevState, confirmPasswordError: '비밀번호를 입력해주세요.' }));
             } else if (formState.password !== formState.confirmPassword) {
               setFormState((prevState) => ({ ...prevState, confirmPasswordError: '비밀번호가 일치하지 않습니다.' }));
+            } else if (formState.password === formState.confirmPassword) {
+              setFormState((prevState) => ({ ...prevState, confirmPasswordError: '' }));
             }
           }}
         />
       </div>
       <div className={styles.profile}>
         <div style={{ marginTop: '36px', marginBottom: '36px' }}>프로필 이미지 (선택)</div>
-        <div>
-          <Image src="/image/profile.png" width="128" height="128" />
-        </div>
+        <label className={styles.profileImageLabel}>
+          {/* 프로필 이미지 클릭 영역 */}
+          <input
+            type="file"
+            accept="image/*"
+            style={{ display: 'none' }}
+            className={styles.profileImageInput}
+            onChange={handleProfileImageUpload} // 파일 업로드 이벤트 핸들러 연결
+          />
+          <Image
+            src={formState.profileImage ? URL.createObjectURL(formState.profileImage) : defaultProfileImage}
+            width="128"
+            height="150"
+          />
+        </label>
       </div>
       <div className={styles.submitbutton}>
-        <Button className={styles.button} onClick={handleSubmit} design="filled" disabled={false} text="회원가입" />
+        <Button
+          className={styles.submitbutton}
+          onClick={handleSubmit}
+          design="filled"
+          disabled={false}
+          text="회원가입"
+        />
       </div>
       <div className={styles.login}>
         이미 계정이 있으신가요?
