b0ff7c87c5e545c2ace0eb967733f7e455e27a5d
BRANDY <kimhijin1@naver.com>
2024-04-04 07:13:02 +0900

    Refactor: 로그인 페이지 코드 리팩토링

--- src/app/(hasNotMap)/login/page.tsx
+++ src/app/(hasNotMap)/login/page.tsx
@@ -8,37 +8,88 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import LoginButton from '@/components/UI/LoginButton';
 import axios from 'axios';
+import Toast from '@/components/UI/Toast';
+
+type FormState = {
+  email: string;
+  password: string;
+  emailError: string;
+  passwordError: string;
+  loginFail: boolean;
+};
 
 export default function Login() {
+  const initialFormState: FormState = {
+    email: '',
+    password: '',
+    emailError: '',
+    passwordError: '',
+    loginFail: false,
+  };
+  const [formState, setFormState] = useState<FormState>(initialFormState);
   const router = useRouter();
 
   const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
   const KAKAO_REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
   const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
   const GOOGLE_REDIRECT_URI = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI;
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [emailError, setEmailError] = useState<string>('');
-  const [passwordError, setPasswordError] = useState<string>('');
   const emailRegEx =
     /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
+  const passwordRegEx = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  const submitRequirements =
+    formState.email &&
+    formState.password &&
+    emailRegEx.test(formState.email) &&
+    passwordRegEx.test(formState.password);
+
+  const validateEmail = (email: string): string => {
+    if (email.length < 1) {
+      return '이메일을 입력해주세요.';
+    } else if (!emailRegEx.test(email)) {
+      return '올바른 이메일 형식으로 입력해주세요.';
+    }
+    return '';
+  };
+
+  const validatePassword = (password: string): string => {
+    if (password.length < 1) {
+      return '비밀번호를 입력해주세요.';
+    } else if (!passwordRegEx.test(password)) {
+      return '영문 대소문자, 숫자, 특수 문자 포함 8자 이상 입력해주세요.';
+    }
+    return '';
+  };
+
+  const handleBlueEmail = () => {
+    setFormState((prevState) => ({
+      ...prevState,
+      emailError: validateEmail(prevState.email),
+    }));
+  };
+
+  const handleBluePassword = () => {
+    setFormState((prevState) => ({
+      ...prevState,
+      passwordError: validatePassword(prevState.password),
+    }));
+  };
 
   const handleLogin = async () => {
+    setFormState((prevState) => ({
+      ...prevState,
+      loginFail: false,
+    }));
+    if (!submitRequirements) {
+      handleBlueEmail();
+      handleBluePassword();
+      return;
+    }
     try {
-      if (!email && !password) {
-        setEmailError('이메일을 입력하세요.');
-        setPasswordError('비밀번호를 입력하세요.');
-        return;
-      } else if (!emailRegEx.test(email)) {
-        setEmailError('올바른 이메일 형식으로 입력해주세요.');
-        return;
-      } else {
-        setEmailError('');
-        setPasswordError('');
-      }
-
       // 로그인 요청 보내기
-      const response = await axios.post('/api/login', { email, password });
+      const response = await axios.post('/api/login', {
+        email: formState.email,
+        password: formState.password,
+      });
 
       // 로그인 성공
       console.log('로그인 성공:', response.data);
@@ -50,10 +101,13 @@ export default function Login() {
       // 여기에서 로그인 성공 시 다른 처리를 할 수 있습니다.
     } catch (error) {
       // 로그인 실패
-      console.error('로그인 실패:', error.response?.data || error.message);
-      console.log('Email:', email);
-      console.log('Password:', password);
-      console.log('emailcheck:', emailRegEx.test(email));
+      setFormState((prevState) => ({
+        ...prevState,
+        loginFail: true,
+      }));
+      console.log('Email:', formState.email);
+      console.log('Password:', formState.password);
+      console.log('emailcheck:', emailRegEx.test(formState.email));
     }
   };
 
@@ -74,19 +128,11 @@ export default function Login() {
             className={styles.input}
             labelText="아이디"
             id="email"
-            valueProp={email}
-            onChangeProp={setEmail}
-            hasError={!!emailError}
-            helpMessage={emailError}
-            onBlur={() => {
-              if (email.length < 1) {
-                setEmailError('이메일을 입력하세요.');
-              } else if (email.length > 0 && emailRegEx.test(email)) {
-                setEmailError('');
-              } else if (!emailRegEx.test(email)) {
-                setEmailError('올바른 이메일 형식으로 입력해주세요.');
-              }
-            }}
+            valueProp={formState.email}
+            onChangeProp={(value) => setFormState((prevState) => ({ ...prevState, email: value }))}
+            hasError={!!formState.emailError}
+            helpMessage={formState.emailError}
+            onBlur={handleBlueEmail}
           />
         </div>
 
@@ -95,17 +141,11 @@ export default function Login() {
           labelText="비밀번호"
           type="password"
           id="password"
-          valueProp={password}
-          onChangeProp={setPassword}
-          hasError={!!passwordError}
-          helpMessage={passwordError}
-          onBlur={() => {
-            if (password.length < 1) {
-              setPasswordError('비밀번호를 입력하세요.');
-            } else if (password.length > 0) {
-              setPasswordError('');
-            }
-          }}
+          valueProp={formState.password}
+          onChangeProp={(value) => setFormState((prevState) => ({ ...prevState, password: value }))}
+          hasError={!!formState.passwordError}
+          helpMessage={formState.passwordError}
+          onBlur={handleBluePassword}
         />
       </div>
       <div>
@@ -132,6 +172,7 @@ export default function Login() {
           <div className={styles.underline}>회원가입</div>
         </Link>
       </div>
+      {formState.loginFail && <Toast type="error" text="로그인에 실패하였습니다." />}
     </div>
   );
 }
