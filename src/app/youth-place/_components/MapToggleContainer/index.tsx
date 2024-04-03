11961fa78127ff029a97180f372466d50a28ea8b
강수정 <kngsujng@naver.com>
2024-04-04 00:38:52 +0900

    Design: 청년정책 지도 페이지 마크업 수정

--- src/app/youth-place/_components/MapToggleContainer/index.tsx
+++ src/app/youth-place/_components/MapToggleContainer/index.tsx
@@ -1,29 +1,12 @@
+import styles from './MapToggleContainer.module.scss';
 import MapContainer from '../MapContainer';
 import ScrollablePlaces from '../ScrollablePlaces';
-import styles from './MapToggleContainer.module.scss';
+import SearchResultHead from '../SearchResultHead';
 
 export default function MapToggleContainer() {
   return (
     <>
-      <div className={styles.header}>
-        <h2>
-          청년 공간 검색 결과 <span>nnn</span>건
-        </h2>
-        <div className={styles.radioWrapper}>
-          <label>
-            <input type="radio" name="sort_place" value="조회순" />
-            <button>조회순</button>
-          </label>
-          <label>
-            <input type="radio" name="sort_place" value="추천순" />
-            <button>추천순</button>
-          </label>
-          <label>
-            <input type="radio" name="sort_place" value="가나다순" />
-            <button>가나다순</button>
-          </label>
-        </div>
-      </div>
+      <SearchResultHead />
       <section className={styles.wrapper}>
         <div className={styles.scrollablePlaces}>
           <ScrollablePlaces />
