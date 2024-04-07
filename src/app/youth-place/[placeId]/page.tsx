685f2d301de09534971e9f8319133e150ed1e5fe
강수정 <kngsujng@naver.com>
2024-04-07 23:24:33 +0900

    Design: 청년공간 이미지 캐러셀 컴포넌트 생성

--- src/app/youth-place/[placeId]/page.tsx
+++ src/app/youth-place/[placeId]/page.tsx
@@ -1,10 +1,12 @@
 import styles from './page.module.scss';
 import PlaceMainInfo from '../_components/PlaceMainInfo';
+import Gallery from '../_components/Gallery';
 
 export default function PlaceDetailPage() {
   return (
     <section className={styles.pageContainer}>
       <PlaceMainInfo />
+      <Gallery />
     </section>
   );
 }
