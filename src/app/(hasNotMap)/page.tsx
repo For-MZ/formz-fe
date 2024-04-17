e66b8cab9e4b9e46dd9baab6f2a2a87effa6cc8c
강동욱 <ehddnr8563@gmail.com>
2024-04-17 19:49:07 +0900

    Remove

--- src/app/(hasNotMap)/page.tsx
+++ src/app/(hasNotMap)/page.tsx
@@ -17,51 +17,13 @@ import Skeleton from 'react-loading-skeleton';
 
 export default function HomePage() {
   const [loading] = useState(true);
-  const [isOpenAlert, onOpenAlert, onCloseAlert] = useModal();
-  const [isOpenConfirm, onOpenConfirm, onCloseConfirm] = useModal();
   const [category, setCategory] = useState([]);
-  console.log(category);
 
   return (
     <>
       <TextField labelText="제목" id="title" name="title" placeholder="제목을 입력해주세요." />
       <TextField labelText="본문" placeholder="본문을 입력해주세요." />
       <TextField labelText="에러 테스트" hasError helpMessage="헬프 에러 미시지" />
-
-      <DropDown
-        multiple
-        options={['전체', '정책', '공간', '주택', '커뮤니티']}
-        defaultValue={'전체'}
-        placeholder="카테고리를 선택해주세요."
-      />
-      <p>선택된 카테고리: {category}</p>
-      <button onClick={onOpenAlert}>Alert 오픈</button>
-      {isOpenAlert && (
-        <Alert onClose={onCloseAlert} heading="alert 헤딩">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus commodi inventore
-          delectus repellendus harum, reprehenderit est ad nesciunt aspernatur eligendi repellat
-          maiores quibusdam pariatur tempora neque quidem. Quidem, ex quibusdam!
-        </Alert>
-      )}
-      <button onClick={onOpenConfirm}>Confirm 오픈</button>
-      {isOpenConfirm && (
-        <Confirm
-          onConfirm={() => {
-            console.log('삭제 수락');
-            onCloseConfirm();
-          }}
-          onCancel={() => {
-            console.log('삭제 취소');
-            onCloseConfirm();
-          }}
-          heading="컨펌 헤딩"
-          rightButtonText="삭제"
-        >
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, culpa distinctio
-          tempore aut praesentium possimus provident, ea eveniet alias ullam nobis voluptas quod
-          commodi iusto eligendi consequatur odit voluptatibus. Sequi!
-        </Confirm>
-      )}
       <Checkbox isNotValid onChange={() => {}} />
       <Divider style="none" />
       <Divider text="text" style="text" />
