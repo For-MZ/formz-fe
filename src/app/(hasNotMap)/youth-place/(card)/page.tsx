99a83f9bff7a1d9fb11f60d1b2f974253f9af914
강수정 <kngsujng@naver.com>
2024-04-20 17:13:19 +0900

    Design: 청년공간 배너 마크업

--- src/app/(hasNotMap)/youth-place/(card)/page.tsx
+++ src/app/(hasNotMap)/youth-place/(card)/page.tsx
@@ -2,11 +2,12 @@ import styles from './page.module.scss';
 import Filter from '../_components/Filter';
 import ToggleButton from '../_components/ToggleButton';
 import FilterablePlaces from '../_components/FilterablePlaces';
+import Banner from '../_components/Banner';
 
 export default function PlacePage() {
   return (
     <section className={styles.pageContainer}>
-      <h3>청년 공간</h3>
+      <Banner />
       <Filter />
       <ToggleButton defaultView="card" />
       <FilterablePlaces />
