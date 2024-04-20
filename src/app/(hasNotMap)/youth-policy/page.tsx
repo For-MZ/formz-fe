4ee8ce972a85758bbf3eba9307ae2741bc9eb9c6
강수정 <kngsujng@naver.com>
2024-04-20 18:37:47 +0900

    Design: 청년정책 배너 마크업

--- src/app/(hasNotMap)/youth-policy/page.tsx
+++ src/app/(hasNotMap)/youth-policy/page.tsx
@@ -1,10 +1,12 @@
 import styles from './page.module.scss';
 import Filter from './_components/Filter';
 import FilterablePolicies from './_components/FilterablePolicies';
+import Banner from './_components/Banner';
 
 export default function PolicyPage() {
   return (
     <section className={styles.pageWrapper}>
+      <Banner />
       <Filter />
       <FilterablePolicies />
     </section>
