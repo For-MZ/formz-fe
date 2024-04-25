f8e7d1fbcd6c0c3944158a88e01e2725d6d411b1
BRANDY <kimhijin1@naver.com>
2024-04-25 23:52:48 +0900

    Feat: 네이버 지도 기능 구현중

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,7 +2,6 @@
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 /*global naver*/
-
 import Script from 'next/script';
 
 declare global {
@@ -14,14 +13,43 @@ declare global {
 
 export default function Map() {
   const initNaverMap = () => {
-    const mapContainer = document.getElementById('map');
-    const mapOption = {
-      center: new window.naver.maps.LatLng(37.3595704, 127.105399),
-      zoom: 10,
+    const mapOptions = {
+      center: new window.naver.maps.LatLng(37.3595704, 127.105399), // 초기 중심 좌표 설정
+      zoom: 10, // 초기 확대 수준 설정
     };
-    new window.naver.maps.Map(mapContainer, mapOption);
-  };
+    const map = new window.naver.maps.Map('map', mapOptions);
 
+    const currentLocationButton = new window.naver.maps.ZoomControl();
+
+    // map.controls.push()를 사용하여 컨트롤 추가
+    map.addControl(currentLocationButton, window.naver.maps.Position.TOP_RIGHT);
+
+    // 현재 위치 버튼 클릭 시 이벤트 핸들러 설정
+    window.naver.maps.Event.addListener(currentLocationButton, 'click', () => {
+      // 현재 위치를 얻어오는 함수
+      const getCurrentLocation = () => {
+        return new Promise((resolve, reject) => {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+              resolve(position.coords);
+            }, reject);
+          } else {
+            reject(new Error('Geolocation is not supported by this browser.'));
+          }
+        });
+      };
+
+      // 현재 위치를 얻어와서 지도 이동
+      getCurrentLocation()
+        .then((coords) => {
+          const currentLocation = new window.naver.maps.LatLng(coords.latitude, coords.longitude);
+          map.setCenter(currentLocation);
+        })
+        .catch((error) => {
+          console.error('Error getting current location:', error.message);
+        });
+    });
+  };
   return (
     <>
       <Script
