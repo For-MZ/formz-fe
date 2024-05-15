66a96e2753d7b6f30d85cc715e47afb38807217e
BRANDY <kimhijin1@naver.com>
2024-05-15 22:59:29 +0900

    Feat: 공공단지 데이터 관련 이슈 테스트

--- src/app/(hasMap)/test/page.tsx
+++ src/app/(hasMap)/test/page.tsx
@@ -5,71 +5,43 @@ const FetchData = () => {
   const [data, setData] = useState(null); // 초기 상태를 null로 설정
 
   useEffect(() => {
-    const fetchData = async (brtcCode, signguCode) => {
-      try {
-        const response = await fetch(
-          `https://apis.data.go.kr/1613000/HWSPR04/rentalHouseGwList?serviceKey=${process.env.NEXT_PUBLIC_SERVICE_KEY}&brtcCode=${brtcCode}&signguCode=${signguCode}&numOfRows=10&pageNo=1`,
-        );
-
+    fetch(
+      `https://apis.data.go.kr/B552555/lhLeaseInfo1/lhLeaseInfo1?serviceKey=${process.env.NEXT_PUBLIC_SERVICE_KEY}&PG_SZ=5000&PAGE=1`,
+    )
+      .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-
-        const json = await response.json(); // 응답을 JSON 형태로 파싱
-        return json.response.body; // body 부분만 반환
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-      }
-    };
-
-    const brtcCodes = [11, 26];
-    const signguCodesMap = {
-      11: [440, 470, 500, 530, 545, 560, 590, 620, 650, 680, 710, 740],
-      26: [110, 140, 170, 200, 230, 260, 290, 320, 350, 380, 410, 440, 470, 500, 530, 710],
-    };
-
-    const fetchDataSequentially = async () => {
-      const allData = [];
-      for (const brtcCode of brtcCodes) {
-        const signguCodes = signguCodesMap[brtcCode];
-        for (const signguCode of signguCodes) {
-          const data = await fetchData(brtcCode, signguCode);
-          allData.push(data);
-        }
-      }
-      setData(allData.flat());
-      console.log(allData.flat()); // 데이터 로깅은 setData 이후에 실행
-    };
-
-    fetchDataSequentially();
+        return response.json(); // 응답을 JSON 형태로 파싱
+      })
+      .then((json) => {
+        setData(json[1].dsList); // dsList 배열에 있는 데이터 저장
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
   }, []);
 
-  if (!data) return <div>Loading...</div>; // 데이터가 없는 경우 로딩 중을 표시
+  if (!data) return <div>No data available</div>;
 
   return (
     <div>
       <ul>
-        {data.map((group, groupIndex) => (
-          <li key={groupIndex}>
-            <h4>Group {groupIndex + 1}</h4>
-            <ul>
-              {group.item.map((item, index) => (
-                <li key={index}>
-                  <h3>건물이름: {item.hsmpNm}</h3>
-                  <p>임대타입: {item.suplyTyNm}</p>
-                  <p>건물종류: {item.houseTyNm}</p>
-                  <p>공급처: {item.insttNm}</p>
-                  <p>공급면적: {item.suplyPrvuseAr}</p>
-                  <p>전용면적: {item.suplyCmnuseAr}</p>
-                  <p>주소: {item.rnAdres}</p>
-                  <p>
-                    보증금: {item.bassRentGtn ? item.bassRentGtn.toLocaleString() : 'N/A'} 원, 월세:
-                    {item.bassMtRntchrg ? item.bassMtRntchrg.toLocaleString() : 'N/A'} 원
-                  </p>
-                </li>
-              ))}
-            </ul>
+        {data.map((item, index) => (
+          <li key={index}>
+            <h3>건물이름: {item.SBD_LGO_NM}</h3>
+            <p>임대타입: {item.AIS_TP_CD_NM}</p>
+            <p>건물종류: {item.HSH_CNT}</p>
+            <p>공급처: {item.ARA_NM}</p>
+            <p>공급면적: {item.DDO_AR}</p>
+            <p>전용면적: {item.LS_GMY}</p>
+            <p>
+              주소: {item.ARA_NM} {item.SBD_LGO_NM}
+            </p>
+            <p>
+              보증금: {item.RFE.toLocaleString()} 원, 월세:
+              {item.SUM_HSH_CNT.toLocaleString()} 원
+            </p>
           </li>
         ))}
       </ul>
